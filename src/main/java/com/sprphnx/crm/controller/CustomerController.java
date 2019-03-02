61b9ac9df2062110b5e968037fb5a2655da20b1d
sprphnx <sprphnx@outlook.com>
2019-03-02 17:54:11 +0300

    Initial commit

---
+++ src/main/java/com/sprphnx/crm/controller/CustomerController.java
@@ -0,0 +1,19 @@
+package com.sprphnx.crm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@RequestMapping("/list")
+	public String listCustomer(Model model) {
+		
+		return "customerList";
+		
+		
+	}
+	
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Welcome</title>
+	</head> 
+	<body>
+		<a href="customer/list">Click to enter</a>
+	</body>
+</html>
